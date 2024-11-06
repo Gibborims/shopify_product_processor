464c97217e1919b30723bc930d34be64f853af9b
Francis Nartey <francis.nartey@Franciss-MacBook-Air.local>
2024-11-06 20:22:08 +0000

    rewrite unit test for processor service class

--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -1,18 +1,29 @@
 # frozen_string_literal: true
 
-# spec/processor_spec.rb
+# # frozen_string_literal: true
+
+# spec/services/shopify_processor/processor_spec.rb
 require 'spec_helper'
 
 RSpec.describe ShopifyProcessor::Processor do
-  let(:processor) { described_class.new }
   let(:mock_products) do
     [
-      {
-        id: 1,
-        title: 'Product 1',
-        description: 'Original description 1'
-      }
+      { id: 1, title: 'Product 1', description: 'This is the original description for Product 1.' },
+      { id: 2, title: 'Product 2', description: 'This is the original description for Product 2.' }
     ]
+  end
+
+  let(:mock_enhanced_descriptions) do
+    ['Enhanced description for Product 1.', 'Enhanced description for Product 2.']
+  end
+
+  let(:subject) { described_class.new }
+
+  describe '.run' do
+    it 'delegates to the instance method' do
+      expect_any_instance_of(described_class).to receive(:run)
+      described_class.run
+    end
   end
 
   describe '#run' do
@@ -20,18 +31,57 @@ RSpec.describe ShopifyProcessor::Processor do
       allow(ShopifyProcessor::Services::ProductFetcher).to receive(:fetch_all)
         .and_return(mock_products)
       allow(ShopifyProcessor::Services::DescriptionEnhancer).to receive(:process)
-        .and_return('Enhanced description 1')
+        .with(mock_products[0][:description]).and_return(mock_enhanced_descriptions[0])
+      allow(ShopifyProcessor::Services::DescriptionEnhancer).to receive(:process)
+        .with(mock_products[1][:description]).and_return(mock_enhanced_descriptions[1])
     end
 
-    it 'creates CSV file with processed descriptions' do
-      processor.run
-      csv_files = Dir.glob('product_descriptions_*.csv')
-      expect(csv_files).not_to be_empty
+    it 'fetches all products' do
+      expect(ShopifyProcessor::Services::ProductFetcher).to receive(:fetch_all)
+        .and_return(mock_products)
+      subject.run
+    end
 
-      content = CSV.read(csv_files.last)
-      expect(content[0]).to eq(['Original Description', 'Enhanced Description'])
-      expect(content[1][0]).to eq('Original description 1')
-      expect(content[1][1]).to eq('Enhanced description 1')
+    it 'processes the products' do
+      expect(subject).to receive(:process_products).with(mock_products)
+      subject.run
+    end
+  end
+
+  describe '#process_products' do
+    let(:csv_double) { instance_double(CSV) }
+    let(:csv_file) { "product_descriptions_#{Time.now.strftime('%Y%m%d_%H%M%S')}.csv" }
+
+    before do
+      allow(CSV).to receive(:open).with(csv_file, 'wb').and_yield(csv_double)
+      allow(csv_double).to receive(:<<)
+
+      # Mock each call to DescriptionEnhancer.process to return the correct enhanced description
+      allow(ShopifyProcessor::Services::DescriptionEnhancer).to receive(:process)
+        .with(mock_products[0][:description]).and_return(mock_enhanced_descriptions[0])
+      allow(ShopifyProcessor::Services::DescriptionEnhancer).to receive(:process)
+        .with(mock_products[1][:description]).and_return(mock_enhanced_descriptions[1])
+    end
+
+    it 'creates a CSV file with the original and enhanced descriptions' do
+      expect(CSV).to receive(:open).with(csv_file, 'wb')
+      expect(csv_double).to receive(:<<).with(['Original Description', 'Enhanced Description'])
+      expect(csv_double).to receive(:<<).with([mock_products[0][:description],
+                                               mock_enhanced_descriptions[0]])
+      expect(csv_double).to receive(:<<).with([mock_products[1][:description],
+                                               mock_enhanced_descriptions[1]])
+
+      subject.send(:process_products, mock_products)
+    end
+  end
+
+  describe '#csv_records' do
+    it 'returns an array of CSV records' do
+      product = { description: 'Original description' }
+      enhanced_description = 'Enhanced description'
+      expect(subject.send(:csv_records, product,
+                          enhanced_description)).to eq(['Original description',
+                                                        'Enhanced description'])
     end
   end
 end
